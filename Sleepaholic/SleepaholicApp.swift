ad082144ee4a64964e8ed0f6bb30094c028cd2ea
dylen400mh <dylenbelanger2004@gmail.com>
2025-09-13 16:09:06 -0400

    Initial Commit

---
+++ Sleepaholic/SleepaholicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepaholicApp.swift
+//  Sleepaholic
+//
+//  Created by Dylen Belanger on 2025-09-13.
+//
+
+import SwiftUI
+
+@main
+struct SleepaholicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
