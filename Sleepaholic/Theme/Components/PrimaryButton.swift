dc02860e326570027c4f57afad372f9433be7741
Dylen Belanger <dylenbelanger2004@gmail.com>
2025-10-14 19:54:02 -0400

    - Create PrimaryButton component

---
+++ Sleepaholic/Theme/Components/PrimaryButton.swift
@@ -0,0 +1,83 @@
+//
+//  PrimaryButton.swift
+//  Sleepaholic
+//
+//  Created by Dylen Belanger on 2025-10-14.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    enum Size {
+        case regular
+        case small
+    }
+
+    let title: String
+    let icon: Image?
+    let size: Size
+    let isDisabled: Bool
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            HStack(spacing: 8) {
+                if let icon = icon {
+                    icon
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                }
+
+                Text(title)
+                    .font(font)
+                    .foregroundColor(.white100)
+            }
+            .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, verticalPadding)
+            .frame(width: width, height: height)
+            .background(Gradients.main)
+            .cornerRadius(100)
+            .applyShadow(ShadowStyle.elevation)
+            .opacity(isDisabled ? 0.5 : 1)
+        }
+        .buttonStyle(.plain)
+        .disabled(isDisabled)
+    }
+
+    // MARK: - Layout constants
+    private var width: CGFloat {
+        switch size {
+        case .regular: return 342
+        case .small: return 147
+        }
+    }
+
+    private var height: CGFloat {
+        switch size {
+        case .regular: return 56
+        case .small: return 48
+        }
+    }
+
+    private var horizontalPadding: CGFloat {
+        switch size {
+        case .regular: return 24
+        case .small: return 16
+        }
+    }
+
+    private var verticalPadding: CGFloat {
+        switch size {
+        case .regular: return 16
+        case .small: return 12
+        }
+    }
+
+    private var font: Font {
+        switch size {
+        case .regular: return .body1Semi
+        case .small: return .body2Semi
+        }
+    }
+}
