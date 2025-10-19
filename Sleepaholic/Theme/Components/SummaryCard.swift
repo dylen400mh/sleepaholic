ad76faf1dbb3fdae625509fbf1f6fef09e088300
Dylen Belanger <dylenbelanger2004@gmail.com>
2025-10-19 17:54:04 -0400

    - Style SummaryCard component for sleep quality + streak

--- Sleepaholic/Theme/Components/SummaryCard.swift
+++ Sleepaholic/Theme/Components/SummaryCard.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct SummaryCard: View {
+    let icon: String
+    let title: String
+    let subtitle: String
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 8) {
+            Gradients.main
+                .mask(Image(systemName: icon)
+                        .resizable()
+                        .scaledToFit())
+                .frame(width: 28, height: 28)
+
+            VStack(alignment: .leading, spacing: 0) {
+                Text(title)
+                    .font(.body1Semi)
+                    .foregroundColor(.white100)
+                Text(subtitle)
+                    .font(.body3)
+                    .foregroundColor(.white80)
+            }
+        }
+        .padding(12)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color.main80)
+        .cornerRadius(12)
     }
 }
 
-#Preview {
-    SummaryCard()
-}
+
